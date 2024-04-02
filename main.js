3601e727eddf26da4f31dcd64c332359038766f5
Danii9790 <162893232+Danii9790@users.noreply.github.com>
2024-04-02 13:06:20 +0500

    Todos_list

---
+++ main.js
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todolist = [];
+let condition = true;
+console.log(chalk.redBright.bold("\n\tWelcome tO Todo_list Application\n"));
+while (condition) {
+    let addTodos = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: "Enter your new Todos"
+        }
+    ]);
+    todolist.push(addTodos.task);
+    console.log(`${addTodos.task} Your Todos list add sucessfully.`);
+    let addMoreTodos = await inquirer.prompt([{
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more Todos",
+            default: "false"
+        }
+    ]);
+    condition = addMoreTodos.addMore;
+}
+console.log("Your Updated Todos list: ", todolist);
